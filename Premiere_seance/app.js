f67c8663e1ce4c1d1ba2b1e2bd4dc600ef98394b
Laxisss <giroudlucas01@gmail.com>
2023-11-10 11:36:42 +0100

    [Edit] Mise en place du serveur

--- Premiere_seance/app.js
+++ Premiere_seance/app.js
@@ -1,1 +1,22 @@
 //Définition des routes
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gaumont_pathe'
+});
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+
+
+
+app.listen(3000, () => console.log("WebService en écoute sur le port 3000"));
