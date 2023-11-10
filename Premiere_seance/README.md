a14a381c7c290c19917ca2f2624514c9dd54c64b
Laxisss <giroudlucas01@gmail.com>
2023-11-10 15:50:39 +0100

    [fix] basepath of swagger + readme

--- Premiere_seance/README.md
+++ Premiere_seance/README.md
@@ -12,6 +12,11 @@ Pour lancer le service
 
 Le service écoute sur le port 3000 (http://localhost:3000)
 
+## Mysql
+Pour Installer la base de données, vous devez disposer d'un logiciel tel que xampp ou une alternative.
+Le service va essayer de se connecter a root@localhost sur la base de données gaumont_pathe.
+Une fois xampp lancé, importez le fichier gaumont_pathe.sql dans le SGBD (en ligne de commande ou par interface), il devrait créer la base de données ainsi que la table et ses données tout seul.
+
 # Nodejs
 Les paramètres de body lors des requêtes POST, PUT, et PATCH sont transmisent sous forme de multipart/form-data et non en application.json
 
--- Premiere_seance/gaumont_pathe.sql
+++ Premiere_seance/gaumont_pathe.sql
@@ -2,14 +2,17 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
+CREATE DATABASE gaumont_pathe;
+
+USE gaumont_pathe;
 -- Hôte : 127.0.0.1
--- Généré le : ven. 10 nov. 2023 à 11:20
+-- Généré le : ven. 10 nov. 2023 à 15:43
 -- Version du serveur : 10.4.21-MariaDB
 -- Version de PHP : 7.4.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+01:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -36,6 +39,14 @@ CREATE TABLE `films` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
+-- Déchargement des données de la table `films`
+--
+
+INSERT INTO `films` (`id_film`, `title`, `description`, `release_date`, `note`) VALUES
+(1, 'Free Guy', 'Un pnj dans un monde jeu vidéaliste', '2023-11-01', 4),
+(2, 'Moi moche et méchant', 'je s\'appelle gru', '2020-11-10', 3);
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -53,7 +64,7 @@ ALTER TABLE `films`
 -- AUTO_INCREMENT pour la table `films`
 --
 ALTER TABLE `films`
-  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
