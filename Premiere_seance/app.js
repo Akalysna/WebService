5fac53efd74a993c668214acfc42286b1886322b
Laxisss <giroudlucas01@gmail.com>
2023-11-10 15:20:48 +0100

    [edit] swagger via jsdoc

--- Premiere_seance/app.js
+++ Premiere_seance/app.js
@@ -66,7 +66,7 @@ app.get('/films', (req, res) => {
  * @route GET /films/:id
  * @param {int} id Id du film a récupérer
  * @group Film - Opération à propos des films
- * @returns Objet contenant les détails du films
+ * @returns Objet contenant les détails du film
  */
 app.get('/films/:id', (req, res) => {
   // query le film d'id :id
@@ -82,12 +82,12 @@ app.get('/films/:id', (req, res) => {
 /**
  * Cette route insère un film avec les données fournies
  * @route POST /films
- * @param {string} title titre de film
- * @param {string} description Id du film a récupérer
- * @param {string} release_date Id du film a récupérer
- * @param {int} note Id du film a récupérer
+ * @param {string} title Titre du film
+ * @param {string} description Description du film a insérer
+ * @param {string} release_date Date de parution du film a insérer
+ * @param {int} note note / 5 du film a insérer
  * @group Film - Opération à propos des films
- * @returns Objet contenant les détails du films
+ * @returns Objet contenant les détails du film inséré
  */
 app.post('/films', upload.fields([]), (req, res) => {
   // ajoute un film
@@ -100,10 +100,32 @@ app.post('/films', upload.fields([]), (req, res) => {
   })
 })
 
+/**
+ * Cette route écrase un film avec les données fournies
+ * @route PUT /films/:id
+ * @param {int} id Id du film a écraser
+ * @param {string} title Nouveau titre du film
+ * @param {string} description Nouvelle Description du film
+ * @param {string} release_date Nouvelle date de parution du film
+ * @param {int} note Nouvelle note du film
+ * @group Film - Opération à propos des films
+ * @returns Objet contenant les détails du film écrasé
+ */
 app.put('/films/:id', upload.fields([]), (req, res) => {
   // ecrase un film
 })
 
+/**
+ * Cette route corrige un film avec les données fournies
+ * @route PATCH /films/:id
+ * @param {int} id Id du film a corriger
+ * @param {string} title Optionnel - Nouveau titre du film
+ * @param {string} description Optionnel - Nouvelle Description du film
+ * @param {string} release_date Optionnel - Nouvelle date de parution du film
+ * @param {int} note Optionnel - Nouvelle note du film
+ * @group Film - Opération à propos des films
+ * @returns Objet contenant les détails du film corrigé
+ */
 app.patch('/films/:id', upload.fields([]), (req, res) => {
   // corrige un champ d'un film
   services.patchFilm(req.body, req.params.id)
@@ -116,6 +138,13 @@ app.patch('/films/:id', upload.fields([]), (req, res) => {
   })
 })
 
+/**
+ * Cette route supprime un film selon l'id fourni
+ * @route DELETE /films/:id
+ * @param {int} id Id du film a corriger
+ * @group Film - Opération à propos des films
+ * @returns Objet contenant les détails du film supprimé
+ */
 app.delete('/films/:id', upload.fields([]), (req, res) => {
   // supprime un film
   services.deleteFilm(req.body, req.params.id)
